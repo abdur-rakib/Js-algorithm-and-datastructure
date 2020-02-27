e60ee629d2bb87c8a3fbddcada8e668815166c07
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-02-27 17:20:05 +0600

    Math power

---
+++ Easy Ones/math-power.js
@@ -0,0 +1,14 @@
+
+// # Math Power
+
+// ## The problem
+// Take two numbers from the users. Calculate the result of second number power of the first number.
+
+// ## Hints
+// To power, you will need to use two asterisks symbols (two multiplication symbols). For example 4 to the power 3 will be
+
+var num = 2;
+var pow = 3;
+
+var res = num ** pow;
+console.log(res);
