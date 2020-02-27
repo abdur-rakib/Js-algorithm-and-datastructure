d1c3dd9578c86e7830bbb6aa8027ba87bb9d44dc
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-02-27 17:18:08 +0600

    Floor division

---
+++ Easy Ones/floor-division.js
@@ -0,0 +1,14 @@
+
+// # Floor division
+
+// ## The problem
+// Find the floor division of two numbers. 
+
+// ## Hints
+// Floor division means the integer part of a division operation. For example, if you divide 17/5 the quotient will be 3.4.
+
+var a = 17;
+var b = 10;
+
+var floor_division = Math.floor(a/b);
+console.log(floor_division);
