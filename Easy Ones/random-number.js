4d08999c6740e4dcb927bf5a3376d5b067ad630b
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-02-27 17:24:45 +0600

    Random number

---
+++ Easy Ones/random-number.js
@@ -0,0 +1,10 @@
+// # Create a random number
+
+// ## The problem
+// Create a random number between 0 to 10
+
+// ## Hints
+// To create a random number, you have to import a built-in library named random. And then you can call the randint method on it
+
+var randomNum = Math.round(Math.random() * 10);
+console.log(randomNum);
