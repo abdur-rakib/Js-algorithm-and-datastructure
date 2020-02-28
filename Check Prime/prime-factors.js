dc6192dacfb348ed3a173270f51685b0d347ec6a
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-02-28 12:38:00 +0600

    Prime factors

---
+++ Check Prime/prime-factors.js
@@ -0,0 +1,26 @@
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    } else {
+        for (var i = 2; i <= Math.ceil(num / 2); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+
+
+var num = 48;
+
+for (var i = 2; i <= num; i++) {
+    if (num % i == 0) {
+        var res = isPrime(i);
+        if (res) {
+            console.log(i);
+        }
+    }
+}
